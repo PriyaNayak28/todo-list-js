a21deab20a6244d4c0e2e7f53099acc21deb5d9d
PriyaNayak28 <nayakpriya612@gmail.com>
2024-07-26 22:59:05 +0530

    file added

---
+++ app.js
@@ -0,0 +1,12 @@
+// styling header to change background color and give some bottom border
+const header = document.querySelector('header');
+header.style.backgroundColor = "purple";
+header.style.borderBottom = '3px solid black';
+// styling heading to change font style and color
+const h1 = document.querySelector('h1');
+h1.textContent = "TO-Do-List";
+h1.style.color = "skyBlue";
+// changing the text and make it bold
+const para = document.getElementById('thanks');
+para.innerHTML = "Please visit us again";
+
